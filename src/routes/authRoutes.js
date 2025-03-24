6683ebc37d12b538d52f8d7a15cd800a4a8c532e
callmeAngga <wongfromindo@gmail.com>
2025-03-24 16:26:45 +0700

    add input validations using Zod for login and register

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 const authController = require('../controllers/authController');
 const authMiddleware = require('../middleware/authMiddleware');
+const validateRequest = require('../middleware/validateRequestMiddleware');
+const { registerSchema, loginSchema } = require('../validators/authValidator');
 
-router.post('/register', authController.register);
-router.post('/login', authController.login);
+router.post('/register', validateRequest(registerSchema), authController.register);
+router.post('/login', validateRequest(loginSchema), authController.login);
 router.get('/me', authMiddleware, authController.getMe);
 
 module.exports = router;
