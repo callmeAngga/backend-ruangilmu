67b4c2341fd36e0067064f6e99c4b540f630822f
callmeAngga <wongfromindo@gmail.com>
2025-05-19 12:39:19 +0700

    change cors

--- server.js
+++ server.js
@@ -15,7 +15,7 @@ const path = require('path');
 process.noDeprecation = true;
 
 app.use(cors({ 
-  origin: 'http://127.0.0.1:5500',
+  origin: 'localhost:3000',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
