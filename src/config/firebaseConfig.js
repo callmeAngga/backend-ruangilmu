29a80c2f1e536bac5686a0378caf37e5869c350a
callmeAngga <wongfromindo@gmail.com>
2025-05-21 11:00:16 +0700

    change firebase config again

--- src/config/firebaseConfig.js
+++ src/config/firebaseConfig.js
@@ -1,10 +1,11 @@
 var admin = require("firebase-admin");
 // var serviceAccount = require("./serviceAccountKey.json");
 
+const credentials = JSON.parse(process.env.FIREBASE_KEY);
 
 if (!admin.apps.length) {
     admin.initializeApp({
-      credential: admin.credential.cert(process.env.FIREBASE_KEY),
+      credential: admin.credential.cert(credentials),
     });
   }
 
