e11939a0d7e3eb603aa35bf5b9b0b4c21834e352
callmeAngga <wongfromindo@gmail.com>
2025-05-27 11:01:22 +0700

    meerubah server.js

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const limiter = rateLimit({
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: true,
   // origin: 'http://localhost:5173',
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
