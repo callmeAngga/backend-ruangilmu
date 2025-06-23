bb365f891265accfab32c7b5278c7fd26de0fb89
Angga Prasetyo <131373748+callmeAngga@users.noreply.github.com>
2025-06-23 19:39:06 +0700

    Update server.js

--- server.js
+++ server.js
@@ -35,7 +35,9 @@ app.use(cors({
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(helmet());
+app.use(helmet({
+    crossOriginResourcePolicy: { policy: "cross-origin" }
+}));
 
 // --- Routes ---
 app.use('/auth', authRoutes);
